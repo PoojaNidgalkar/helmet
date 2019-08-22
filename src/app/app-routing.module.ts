1452dbf612b032c23ac465ce8bfb7e203c0e2757
PoojaNidgalkar <poojanidgalkar.7@gmail.com>
2019-08-22 12:11:24 +0530

    d

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageComponent } from './page/page.component';
+import {FormComponent } from './form/form.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'signup',
+    component:SignupComponent 
+  },
+
+  {
+    path:'page', 
+    component:PageComponent 
+  },
+
+  {
+    path:'form', 
+    component:FormComponent 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { EditorModule } from '@tinymce/tinymce-angular';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageComponent } from './page/page.component';
+import {FormComponent } from './form/form.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule }   from '@angular/forms';
+import { SignupComponent } from './signup/signup.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    PageComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    EditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
